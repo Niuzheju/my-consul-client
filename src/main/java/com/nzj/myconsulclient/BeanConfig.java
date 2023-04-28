bd15acdca8617b1387c6f95180b6195c46ea2d71
niuzheju <niuzheju@gmail.com>
2023-04-28 14:54:00 +0800

    调用远程服务

---
+++ src/main/java/com/nzj/myconsulclient/BeanConfig.java
@@ -0,0 +1,21 @@
+package com.nzj.myconsulclient;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author niuzheju
+ * @Date 18:07 2023/4/21
+ */
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
--- src/main/java/com/nzj/myconsulclient/MyConsulClientApplication.java
+++ src/main/java/com/nzj/myconsulclient/MyConsulClientApplication.java
@@ -3,40 +3,27 @@ package com.nzj.myconsulclient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
 public class MyConsulClientApplication {
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private RestTemplate restTemplate;
 
     @RequestMapping("/")
     public String home() {
-        String serviceUrl = serviceUrl();
-        System.out.println(serviceUrl);
-        return "Hello World";
-    }
-
-    public String serviceUrl() {
-        List<ServiceInstance> instances = discoveryClient.getInstances("my-consul-client");
-        if (instances != null && instances.size() > 0) {
-            for (ServiceInstance instance : instances) {
-                System.out.println(instance);
-            }
-            return instances.get(0).getUri().toString();
-        }
-        return null;
+        // 使用RestTemplate调用远程服务
+        String s = restTemplate.getForObject("http://my-consul-client-a/", String.class);
+        return "获取到远程服务my-consul-client-a的结果: " + s;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(MyConsulClientApplication.class, args);
     }
+
 
 }
