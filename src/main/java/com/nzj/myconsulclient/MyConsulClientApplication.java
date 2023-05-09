5dd59b0a448c9f1b6bb9a62fe81da01c46e41387
niuzheju <niuzheju@gmail.com>
2023-05-09 17:40:26 +0800

    分布式配置使用

--- src/main/java/com/nzj/myconsulclient/MyConsulClientApplication.java
+++ src/main/java/com/nzj/myconsulclient/MyConsulClientApplication.java
@@ -3,14 +3,18 @@ package com.nzj.myconsulclient;
 import com.nzj.myconsulclient.config.MyLoadBalancerConfig;
 import com.nzj.myconsulclient.feignclient.MyConsulClientAFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
+@RefreshScope // 用于自动刷新分布式配置属性，需要写在属性所在的类
 @LoadBalancerClient(name = "my-consul-client", configuration = MyLoadBalancerConfig.class)
 @EnableFeignClients
 @RestController
@@ -23,6 +27,15 @@ public class MyConsulClientApplication {
     @Autowired
     private MyConsulClientAFeign myConsulClientAFeign;
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+    /**
+     * RestTemplate调用远程服务
+     */
     @RequestMapping("/")
     public String home() {
         // 使用RestTemplate调用远程服务
@@ -39,6 +52,14 @@ public class MyConsulClientApplication {
         // 使用RestTemplate调用远程服务
         String s = myConsulClientAFeign.home();
         return "使用feign client获取到远程服务my-consul-client-a的结果: " + s;
+    }
+
+    /**
+     * 分布式配置
+     */
+    @RequestMapping("/home3")
+    public String distributeConfig() {
+        return "consul config name: " + name + " age: " + age;
     }
 
     public static void main(String[] args) {
